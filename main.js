faf85928b5701a44821dcf55aeaf24045ef8a781
mathias hansen <mathiasosthan@gmail.com>
2018-03-31 16:05:14 +0200

    added html requirements

--- main.js
+++ main.js
@@ -6,10 +6,21 @@ app.use("/", express.static(__dirname + "/public"))
 app.use((req,res) => {
     res.writeHead(200, { "content-type": "text/html" })
     res.end(`
-        <center>
-            <img style="margin: 32px 0 24px 0" src="/logo.png" width="300px"/>
-            <p>siden er under ombygning</p>
-        </center>
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Siggaard & Hansen Technology</title>
+        </head>
+        <body>
+            <center>
+                <img style="margin: 32px 0 24px 0" src="/logo.png" width="300px"/>
+                <p>siden er under ombygning</p>
+            </center>
+        </body>
+        </html>
     `)
 })
 
