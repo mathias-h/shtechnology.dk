642a0457df826f5541f055e6f7dedb91cf3faea1
mathias hansen <mathiasosthan@gmail.com>
2018-04-04 09:15:41 +0200

    inital commit

--- main.js
+++ main.js
@@ -1,27 +1,24 @@
 const express = require("express")
+const bodyParser = require("body-parser")
 const app = express()
+
 
 app.use("/", express.static(__dirname + "/public"))
 
 app.use((req,res) => {
-    res.writeHead(200, { "content-type": "text/html" })
-    res.end(`
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <title>Siggaard & Hansen Technology</title>
-        </head>
-        <body>
-            <center>
-                <img style="margin: 32px 0 24px 0" src="/logo.png" width="300px"/>
-                <p>siden er under ombygning</p>
-            </center>
-        </body>
-        </html>
-    `)
+    res.sendFile(__dirname + "/index.html")
 })
 
-app.listen(80)
+const fs = require("fs")
+app.post("/message", bodyParser.json(), (req,res) => {
+    const { mail, subject, message } = req.body
+    fs.appendFileSync("/data/messages.txt", `
+        mail: ${mail}
+        subject: ${subject}
+        message: ${message}
+        \n\n
+    `)
+    res.end("OK")
+})
+
+app.listen(8080)
